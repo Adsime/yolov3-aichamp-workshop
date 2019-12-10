422c72426bbfc3a0d6888b2ec350fe0b86beab05
adsime <adrianmelsom@gmail.com>
2019-12-10 23:08:10 +0100

    intro mostly finished in terms of code. Youtube next up

--- src/network.py
+++ src/network.py
@@ -1,9 +1,16 @@
 import cv2 as cv
 import numpy as np
+from copy import deepcopy
 
 weigths_path = "data/weights/"
 cfg_path = "data/cfg/"
 class_path = "data/"
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+font = cv.FONT_HERSHEY_PLAIN
+font_scale = 1
+thickness = 2
 
 def load(weights, cfg):
     weights = weigths_path + weights
@@ -18,47 +25,57 @@ def extract_output_layers(net):
     names = net.getLayerNames()
     return [names[layer[0] - 1] for layer in net.getUnconnectedOutLayers()]
 
-def image_to_blob(image, scale):
-    width, height, _ = image.shape
-    return cv.dnn.blobFromImage(image, scalefactor=scale, size=(416, 416), swapRB=True, crop=False)
+def image_to_blob(image, size=(416, 416)):
+    return cv.dnn.blobFromImage(image, scalefactor=1/255, size=(416, 416), swapRB=True, crop=False)
 
 def forward(net, blob, output_layers):
     net.setInput(blob)
     return net.forward(output_layers)
 
-def process_result(image, outputs, classes, threshold=0.8):
-    width, height, _ = image.shape
+def postprocess(image, outputs, classes, threshold=0.8, NMS=True):
+    height, width, _ = image.shape
+
+    image = deepcopy(image)
 
     class_ids = []
     confidences = []
     boxes = []
 
-    print(len(outputs))
-
     for o in outputs:
-        print(len(o))
         for detection in o:
+            center_x = detection[0] * width
+            center_y = detection[1] * height
+            w = detection[2] * width
+            h = detection[3] * height
+            x = center_x - w/2
+            y = center_y - h/2
+
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if(confidence > threshold):
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
 
-                x = int(center_x - w/2)
-                y = int(center_y - h/2)
+            boxes.append([int(x), int(y), int(w), int(h)])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
 
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-    #indices = cv.dnn.NMSBoxes(boxes, confidences, threshold, 0.0)
 
-    for i, box in enumerate(boxes):
-        x,y,w,h = box
+    indices = cv.dnn.NMSBoxes(boxes, confidences, threshold, 1)
+
+    for i in indices:
+        i = i[0]
+        print(confidences[i])
         label = str(classes[class_ids[i]])
-        cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), 2)
-        cv.putText(image, label, (x, y + 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+        draw_box(image, label, boxes[i])
+        
+    return image
 
-    return boxes, confidence, class_ids
+def convertToCoordinates(x, y, w, h):
+    
+    return [int(x), int(y), int(w), int(h)]
+
+def draw_box(image, label, box):
+    x,y,w,h = box
+    tw, th = cv.getTextSize(label, font, font_scale, thickness=thickness)[0]
+    cv.rectangle(image, (x, y), (x + w, y + h), black, thickness=thickness)
+    cv.rectangle(image, (x, y), (x + tw + 10, y - th - 10), black, cv.FILLED)
+    cv.putText(image, label, (x + 5, y - 5), font, font_scale, (255, 255, 255), thickness=thickness)
